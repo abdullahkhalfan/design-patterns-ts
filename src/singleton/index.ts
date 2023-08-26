c6d3e6a60116673abac79fe4bf1dc96fccfa118e
Abdullah Khalfan <abdullahkhalfan@icloud.com>
2023-08-26 22:04:02 +1000

    Update notes

--- src/singleton/index.ts
+++ src/singleton/index.ts
@@ -3,6 +3,10 @@
  *
  * Ensure a class only has a single instance. Typically used for stuff like
  * logging, settings, caching, etc.
+ *
+ * Note: In JavaScript, there's a much easier alternative to the approach
+ * demonstrated. In Config.ts just export a new instance of the object, so
+ * when imported only that single instance can be used.
  */
 
 import Config from "./Config";
